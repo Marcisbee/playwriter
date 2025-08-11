ee25fa06bab9e96db76559fd482986bd921efb8c
Marcis Bergmanis <marcisbergmanis@gmail.com>
2025-08-11 17:01:34 +0300

    bump

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Download the Playwriter.zip archive
-curl -L -o Playwriter.zip "https://github.com/Marcisbee/playwriter/releases/download/v0.1.5/Playwriter.zip"
+curl -L -o Playwriter.zip "https://github.com/Marcisbee/playwriter/releases/download/v0.1.6/Playwriter.zip"
 
 # Extract the archive to the current directory
 unzip -o Playwriter.zip
